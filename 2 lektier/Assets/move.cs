a358a6b438a5d43c742970ee3dfdfebb37974cc2
Kadel Saleh <whatisthepoll@gmail.com>
2022-10-28 14:05:16 +0200

    Initialize your first game character!

--- 2 lektier/Assets/move.cs
+++ 2 lektier/Assets/move.cs
@@ -1,14 +1,26 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    public string name ="Master";
+    private int level=1;
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log(name);
+        character();
+        level+=1;
+        character();
+    }
+    /// <summary>
+    /// Method for printing the character name and level.
+    /// </summary>
+    void character(){
+        Debug.Log($"My name is {name} and my level is {level}.");
+        level+=1;
+        Debug.Log($"My name is {name} and my new level is {level}. I have leveled up!");
     }
 
     // Update is called once per frame
