519fb7628b74c5d2c5bb4b0e8cfc13b749e3e557
Kadel Saleh <whatisthepoll@gmail.com>
2022-10-31 12:33:31 +0100

    Update for 3.2.
    Walking included.

---
+++ 3.2 lektier/pokemon rigging and inverse kinematics/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private int speed = 5;
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
+    }
+    private void OnMovement(InputValue value){
+        movement = value.Get<Vector2>();
+        if (movement.x != 0 || movement.y != 0){
+            anim.SetBool("IsWalking", true);}
+        else {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+    private void FixedUpdate(){
+        rb.velocity=movement*speed;
+    }
+}
